ce3f763407119833525bb2a7d7cdb3a0574bc438
Vitalii Antoniuk <va@jelasric.com>
2018-12-16 12:23:06 +0200

    close and archive project

--- wordpress/wp-content/themes/wp-slushano/pagination.php
+++
@@ -1,6 +0,0 @@
-<?php /* ?>
-<div class="pagination">
-  <!-- noindex --><?php html5wp_pagination(); ?><!-- /noindex -->
-</div><!-- /pagination -->
-<?php */ ?>
-<a class="load-more styled-block" data-nonce="<?php echo wp_create_nonce('load_posts') ?>" data-current-page-type="<?php $category = get_the_category(); echo $category[0]->slug;?>" data-id="<?php echo $category[0]->cat_ID;?>" data-showposts="10" data-offset="10" data-all-posts-count="<?php echo $category[0]->category_count;?>"> <span class="title">Загрузить еще</span> </a>
